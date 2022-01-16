903fa29ac4e4e0cb382b630914ae67f90234f195
vinciusfersil123 <viniciusfersil@gmail.com>
2022-01-16 19:02:08 -0300

    :plus:adiciona typescript

--- components/Canvas/index.js
+++ components/Canvas/index.js
@@ -1,10 +1,6 @@
 import dynamic from 'next/dynamic'
-
-// Will only import `react-p5` on client-side
 const Sketch = dynamic(() => import("react-p5").then((mod) => {
-  // importing sound lib ONLY AFTER REACT-P5 is loaded
   require('p5/lib/addons/p5.sound');
-  // returning react-p5 default export
   return mod.default
 }), {
   ssr: false
@@ -24,6 +20,5 @@ export default (props) => {
 		x++;
 	};
 
-// Will only render on client-side
 	return <Sketch setup={setup} draw={draw} />;
 };
