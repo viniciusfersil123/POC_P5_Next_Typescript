14c80f0e380c8d12c9c7c7a7870fa1bca50a3a33
Vinícius Fernandes <viniciusfersil@gmail.com>
2022-01-25 21:12:44 -0300

    :construction: commit pre-fork

--- components/Canvas/index.tsx
+++ components/Canvas/index.tsx
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic";
 import p5Types from "p5";
 
-
 const Sketch = dynamic(
   () =>
     import("react-p5").then((mod) => {
@@ -13,44 +12,37 @@ const Sketch = dynamic(
   }
 );
 
-let theta : number;
 let width: number = 710;
 let height: number = 400;
-let branch: any;
+let shapes = [];
+
+let newShape;
+
+let joints = 12;
+let linelength = 64;
+let resolution = 0.06;
+let gravity = 0.094;
+let damping = 0.998;
+let showPath = true;
+let showPendulum = true;
+let showPendulumPath = true;
+let clearScreen = false;
+let bolaBranca;
 
 export default () => {
-  
-    const setup = (p5: p5Types, canvasParentRef: Element) => {
-    p5.createCanvas(width, height).parent(canvasParentRef);
-    branch = function (h : number) {
-      h *= 0.66;
-      if (h > 2) {
-        p5.push();
-        p5.rotate(theta);   
-        p5.line(0, 0, 0, -h); 
-        p5.translate(0, -h);
-        branch(h);      
-        p5.pop();
-        p5.push();
-        p5.rotate(-theta);
-        p5.line(0, 0, 0, -h);
-        p5.translate(0, -h);
-        branch(h);
-        p5.pop();
-      }
-    }
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    bolaBranca = function () {
+      p5.fill(255,0,0);
+      p5.ellipse(p5.windowWidth / 2, p5.windowHeight / 2, 200, 200);
+    };
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    p5.noFill();
+    p5.strokeWeight(1);
   };
 
   const draw = (p5: p5Types) => {
     p5.background(0);
-    p5.frameRate(30);
-    p5.stroke(255);
-    let a : number = (p5.mouseX / width) * 90;
-    theta = p5.radians(a);
-    p5.translate(width/2,height);
-    p5.line(0,0,0,-120);
-    p5.translate(0,-120);
-    branch(120);
+    bolaBranca();
   };
 
   return <Sketch setup={setup} draw={draw} />;
